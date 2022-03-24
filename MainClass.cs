5b48986691ef02c9085722eeb8628c2ccb0b7c86
ViktoD <90144863+ViktoD@users.noreply.github.com>
2022-03-24 09:08:31 +0200

    Add files via upload

---
+++ MainClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace lab3
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            //1)
+            WriteLine("1)------------------------------------");
+            StudentCollection studentCollection = new StudentCollection();
+            studentCollection.AddDefaults();
+            studentCollection.AddStudents(new Student[] { new Student(new Person("Ihor", "Kolotylo", new DateOnly(1999, 10, 23)), Education.SecondEducation, 202) { Exams = new List<Exam>() { new Exam("Java", 4, new DateOnly(2021, 12, 09)), new Exam("Php", 5, new DateOnly(2022, 2, 12)) }, Tests = new List<Test>() { new Test(), new Test("Computer graphic", false), new Test("Database", true) } }, new Student(new Person("Ivan", "Dmytrenko", new DateOnly(2005, 09, 29)), Education.Master, 205) { Exams = new List<Exam>() { new Exam(), new Exam("Database", 3, new DateOnly(2020, 6, 1)) }, Tests = new List<Test>() { new Test() } } });
+            WriteLine(studentCollection);
+            WriteLine("----------------------------------------\n");
+
+
+            //2)
+            WriteLine("2)------------------------------------");
+            WriteLine("--Sort by surname--\n");
+            studentCollection.SortBySurname();
+            WriteLine(studentCollection);
+            WriteLine("--Sort by date--\n");
+            studentCollection.SortByDate();
+            WriteLine(studentCollection);
+            WriteLine("--Sort by grade--\n");
+            studentCollection.SortByGrade();
+            WriteLine(studentCollection.ToShortString());
+            WriteLine("----------------------------------------\n");
+
+            //3)
+            WriteLine("3)------------------------------------");
+            WriteLine("--Max average grade--");
+            WriteLine(studentCollection.MaxGrade);
+            WriteLine("--Students with education Master--");
+            foreach(Student stud in studentCollection.TypeEducation)
+            {
+                WriteLine(stud);
+            }
+            WriteLine("--Group students with mark--");
+            foreach(Student stud in studentCollection.AverageMarkGroup(4.5))
+            {
+                WriteLine(stud);
+            }
+            WriteLine("----------------------------------------\n");
+
+            //4)
+            WriteLine("4)------------------------------------");
+            TestCollections testCollection = new TestCollections(1000000);
+            testCollection.TimeFinding(1000000);
+            WriteLine("----------------------------------------\n");
+
+        }
+    }
+}
---
+++ TestCollections.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace lab3
+{
+    internal class TestCollections
+    {
+        private List<Person> _persons;
+        private List<string> _strings;
+        private Dictionary<Person, Student> _personStudent;
+        private Dictionary<string, Student> _stringStudent;
+
+        public TestCollections(int count)
+        {
+
+            if(_persons == null) { _persons = new List<Person>(); }
+            if(_strings == null) { _strings = new List<string>(); }
+            if(_personStudent == null) { _personStudent = new Dictionary<Person, Student>(); }
+            if(_stringStudent == null) { _stringStudent = new Dictionary<string, Student>(); }
+            
+            for (int i = 1; i<= count; i++)
+            {
+
+                    Person person = GenerateStudent(i).Person;
+                    Student student = GenerateStudent(i);
+                    _persons.Add(person);
+                    _strings.Add(person.ToString());
+                    _personStudent.Add(person, student);
+                    _stringStudent.Add(person.ToString(), student);
+            }
+        }
+
+        public static Student GenerateStudent(int i)
+        {
+            return new Student(new Person("Ivan" + i, "Dmytrenko" + i, new DateOnly(2001, 8, 15)), Education.Bachelor, 301);
+        }
+
+        public void TimeFinding(int count)
+        {
+            int start, end;
+            Person person = GenerateStudent(0).Person;
+            Student student = GenerateStudent(0);
+            WriteLine("\n\n--------------First element---------------\n");
+
+            WriteLine("--------Time in List<Person>----------------\n");
+            start = Environment.TickCount;
+            _persons.Contains(person);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in List<string>---------------\n");
+            start = Environment.TickCount;
+            _strings.Contains(person.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsKey(person);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<string,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _stringStudent.ContainsKey(person.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by value---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsValue(student);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+
+
+            WriteLine("\n\n--------------Central element-----------------\n");
+
+            Person person1 = GenerateStudent(count/2).Person;
+            Student student1 = GenerateStudent(count/2);
+            WriteLine("--------Time in List<Person>----------------\n");
+            start = Environment.TickCount;
+            _persons.Contains(person1);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in List<string>---------------\n");
+            start = Environment.TickCount;
+            _strings.Contains(person1.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsKey(person1);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<string,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _stringStudent.ContainsKey(person1.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by value---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsValue(student1);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+
+
+            WriteLine("\n\n--------------Last element-----------------\n");
+
+            Person person2 = GenerateStudent(count-1).Person;
+            Student student2 = GenerateStudent(count-1);
+            WriteLine("--------Time in List<Person>----------------\n");
+            start = Environment.TickCount;
+            _persons.Contains(person2);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in List<string>---------------\n");
+            start = Environment.TickCount;
+            _strings.Contains(person2.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsKey(person2);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<string,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _stringStudent.ContainsKey(person2.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by value---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsValue(student2);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+
+
+
+            WriteLine("\n\n--------------Element which isn't a collection-----------------\n");
+
+            Person person3 = GenerateStudent(count).Person;
+            Student student3 = GenerateStudent(count);
+            WriteLine("--------Time in List<Person>----------------\n");
+            start = Environment.TickCount;
+            _persons.Contains(person3);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in List<string>---------------\n");
+            start = Environment.TickCount;
+            _strings.Contains(person3.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsKey(person3);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<string,Student> by key---------------\n");
+            start = Environment.TickCount;
+            _stringStudent.ContainsKey(person3.ToString());
+            end = Environment.TickCount;
+            WriteLine(end - start);
+
+            WriteLine("--------Time in Dictionary<Person,Student> by value---------------\n");
+            start = Environment.TickCount;
+            _personStudent.ContainsValue(student3);
+            end = Environment.TickCount;
+            WriteLine(end - start);
+        }
+
+    }
+}
